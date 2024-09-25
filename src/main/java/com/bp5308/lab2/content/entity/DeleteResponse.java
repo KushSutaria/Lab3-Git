2cef4312ae9e6387fdc150de9ba36013640f80bc
Mehil Shah <shahmehil@dal.ca>
2024-09-25 16:22:26 -0300

    chore: set up the code for lab 3

---
+++ src/main/java/com/bp5308/lab2/content/entity/DeleteResponse.java
@@ -0,0 +1,10 @@
+package com.bp5308.lab2.content.entity;
+
+public class DeleteResponse {
+    public String id;
+    public String message;
+    public DeleteResponse(String id, String message) {
+        this.id = id;
+        this.message = message;
+    }
+}
